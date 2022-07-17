565153c01cd9dfdb0f5f5e847542f92c1bed6e0b
werbeth <werbeth1991pbs@gmail.com>
2022-07-17 16:53:08 -0300

    feat: gerador funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
+const displayPassword = document.querySelector(".display");
+const inputQtd = document.querySelector(".input-qtd");
+const chkUppercases = document.querySelector(".chk-uppercases");
+const chkLowercases = document.querySelector(".chk-lowercases");
+const chkNumber = document.querySelector(".chk-numbers");
+const chkSymbols = document.querySelector(".chk-symbols");
+const buttomPassword = document.querySelector(".generate-password");
+
 //função que gera um numero aleatorio, que vai ser usado como código da tabela asc
 function codeAsc(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -21,7 +29,7 @@ function generateSymbol() {
   return symbols[codeAsc(0, symbols.length)];
 }
 //função que gera a senha
-export default function generatePassword(qtd, upper, lower, num, simb) {
+function generatePassword(qtd, upper, lower, num, simb) {
   const password = [];
   qtd = Number(qtd);
 
@@ -32,4 +40,20 @@ export default function generatePassword(qtd, upper, lower, num, simb) {
     simb && password.push(generateSymbol());
   }
   return password.join("").slice(0, qtd);
+}
+
+buttomPassword.addEventListener("click", () => {
+  displayPassword.innerHTML = newPasswordGenerate();
+});
+
+function newPasswordGenerate() {
+  const password = generatePassword(
+    inputQtd.value,
+    chkUppercases.checked,
+    chkLowercases.checked,
+    chkNumber.checked,
+    chkSymbols.checked
+  );
+
+  return password || "Selecione o tipo de caractere...";
 }
